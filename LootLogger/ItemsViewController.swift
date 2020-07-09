096254bef7893a3fee689afda205e52c785a0923
Terrance <terrance@defmethod.io>
2020-07-09 15:37:25 -0400

    add stackview DetailViewController

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -21,6 +21,25 @@ class ItemsViewController: UITableViewController {
   }
   
   
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // If the triggered segue is the "showItem" segue
+    switch segue.identifier {
+    case "showItem":
+      //Figuire out which row was just tapped
+      if let row = tableView.indexPathForSelectedRow?.row {
+        
+        //Get the item associated witht his row and pass it along
+        let item = itemStore.allItems[row]
+        let detailViewController
+          = segue.destination as! DetailViewController
+        detailViewController.item = item
+      }
+    default:
+      preconditionFailure("Unexpected segue identifier.")
+    }
+  }
+  
+  
   //TABLE VIEW FUNCTIONS
   
   //setting number of rows
